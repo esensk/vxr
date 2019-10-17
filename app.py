a1c7e5ca814a0c9fd2f03a7a1531826efaa6544e
esuenu <mogmogkyuuri@gmail.con>
2019-10-17 09:55:24 +0900

    [add] Add a prototype of login function

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # -*- coding:utf-8 -*-
 
 from bottle import route, run
+from bottle import request, template
+
+from settings import DB_PASSWORD
+from dao import Dao
 
 
 @route("/")
@@ -20,12 +24,25 @@ def do_register():
 
 @route("/user/login")
 def login():
-    pass
+    username = request.query.get("user")
+    password = request.query.get("pass")
+
+    username = "" if username is None else username
+    password = "" if password is None else password
+
+    return template("user/login", username=username, password=password)
 
 
 @route("/user/login", method="POST")
 def do_login():
-    pass
+    username = request.forms.get("username")
+    password = request.forms.get("password")
+
+    result = dao.select_user(username, password)
+    if len(result) > 0:
+        return template("user/do_login", username=username, password=password)
+
+    return template("user/failed_to_login")
 
 
 @route("/user/update")
@@ -48,4 +65,6 @@ def do_delete():
     pass
 
 
+dao = Dao(
+    "mysql://docker:{pw}@localhost:3306/vxr".format(pw=DB_PASSWORD))
 run(host="localhost", port=8080, debug=True)
