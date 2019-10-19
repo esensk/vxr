cfca799d3a873b7fdf84775fe7cdbdc84b6e948f
esuenu <mogmogkyuuri@gmail.con>
2019-10-20 01:44:44 +0900

    [chore] Change to actual tables

--- db/sql/001-create-tables.sql
+++ db/sql/001-create-tables.sql
@@ -1,11 +1,23 @@
 ---- drop ----
-drop table if exists test_table;
+drop table if exists t_user;
+
+drop table if exists t_effector;
 
 ---- create ----
-create table if not exists test_table (
+create table if not exists t_user (
+    private_id int(20) auto_increment,
+    public_id varchar(20) not null,
+    password varchar(20) not null,
+    created_at datetime default current_timestamp,
+    latest_updated_at datetime on update current_timestamp,
+    primary key(private_id)
+) default charset = utf8 collate utf8_bin;
+
+create table if not exists t_effector (
     id int(20) auto_increment,
-    name varchar(20) not null,
-    created_at datetime default null,
-    updated_at datetime default null,
-    primary key (id)
-) default charset = utf8 collate = utf8_bin;
+    creator_id (20) not null,
+    code text not null,
+    created_at datetime default current_timestamp,
+    latest_updated_at datetime on update current_timestamp,
+    primary key(id)
+) default charset = utf8 collate utf8_bin;
